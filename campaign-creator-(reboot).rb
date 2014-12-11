7ac94c601f73f25288c2e107ff32ec6549515443
Chris Salvato <csalvato@gmail.com>
2014-12-11 12:15:43 +0000

    Initial Commit

---
+++ campaign-creator-(reboot).rb
@@ -0,0 +1,14 @@
+# Create campaign CSV files
+
+start_time = Time.now
+puts "Starting Script..."
+
+# Create/load Keywords
+	# Provide Seeds
+	# Seeds turn into 
+# Create Campaigns
+# Create Keywords (must specify ad group)
+# Create Ads (must specify ad group)
+
+puts "Script Complete!"
+puts "Time elapsed: #{Time.now - start_time} seconds"
