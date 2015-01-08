4273bb4901b2d5337277fe7101457872b465da2f
Chris Salvato <csalvato@gmail.com>
2015-01-08 13:46:57 -0500

    Created BingAd and BingKeyword classes based on existing AdWords classes

--- campaign-creator-(reboot).rb
+++ campaign-creator-(reboot).rb
@@ -750,6 +750,8 @@ class AdGroup
 	end
 end
 
+
+
 # Class representing a Keyword	
 	# Properties:
 		# => Campaign (Campaign object this keyword belongs to)
@@ -799,6 +801,53 @@ class Keyword
 				output_row << @ad_group.ad_group_status
 			when "Status"
 				output_row << @status
+			else
+				output_row << nil # Must be nil for CSV to be written properly.
+			end
+		end
+
+		return output_row
+	end
+end
+
+class BingKeyword
+	TYPE_STRING = "Keyword"
+
+	def initialize( opts={} )
+		opts = { campaign: nil,
+		 		 ad_group: nil,
+		 		 bid: 0.50,
+		 		 keyword: "",
+		 		 match_type: "Broad",
+		 		 device: "All",
+		 		 status: "Active"}.merge(opts)
+		@campaign = opts[:campaign]
+		@ad_group = opts[:ad_group]
+		@bid = opts[:bid]
+		@keyword = opts[:keyword]
+		@match_type = opts[:match_type]
+		@status = opts[:status]
+	end
+
+	def settingsRow
+		output_row = []
+
+		@campaign.output_row_headers.each do |header|
+			case header
+			when "Type"
+				output_row << TYPE_STRING
+			when "Status"
+				output_row << @status
+			when "Campaign"
+				output_row << @campaign.name
+			when "Ad Group"
+				output_row << @ad_group.name
+			when "Keyword"
+				output_row << @keyword
+			when "Match Type"
+				output_row << @match_type
+			when "Bid"
+				output_row << @bid
 			else
 				output_row << nil # Must be nil for CSV to be written properly.
 			end
@@ -869,6 +918,65 @@ class Ad
 				output_row << @ad_group.ad_group_status
 			when "Status"
 				output_row << @status
+			else
+				output_row << nil # Must be nil for CSV to be written properly.
+			end
+		end
+
+		return output_row
+	end
+end
+
+class BingTextAd
+	# Max Field Lengths (in characters)
+	MAX_AD_TITLE_LENGTH = 25
+	MAX_AD_TEXT_LENGTH = 71
+	MAX_DISPLAY_URL_LENGTH = 35
+	MAX_DESTINATION_URL_LENGTH = 1024
+	TYPE_STRING = "Text Ad"
+
+	def initialize( opts={} )
+		opts = { campaign: nil,
+		  ad_group: nil,
+		  title: "",
+		  text: "",
+		  display_url: "",
+		  destination_url: "",
+		  device_preference: "All",
+		  status: "Active"}.merge(opts)
+		@campaign = opts[:campaign]
+		@ad_group = opts[:ad_group]
+		@title = opts[:title]
+		@text = opts[:text]
+		@display_url = opts[:display_url]
+		@destination_url = opts[:destination_url]
+		@device_preference = opts[:device_preference]
+		@status = opts[:status]
+	end
+
+	def settingsRow
+		output_row = []
+
+		@campaign.output_row_headers.each do |header|
+			case header
+			when "Type"
+				output_row << TYPE_STRING
+			when "Status"
+				output_row << @status
+			when "Campaign"
+				output_row << @campaign.name
+			when "Ad Group"
+				output_row << @ad_group.name
+			when "Title"
+				output_row << @title
+			when "Text"
+				output_row << @text
+			when "Display URL"
+				output_row << @display_url
+			when "Destination URL"
+				output_row << @destination_url
+			when "Device Preference"
+				output_row << @device_preference
 			else
 				output_row << nil # Must be nil for CSV to be written properly.
 			end
