f99028c6536a9082c24ff0754956594e6bfa4f08
Chris Salvato <csalvato@gmail.com>
2015-01-08 23:36:14 -0500

    Create City/State factory for bing.  Testing functions/spreadsheets.  May be ready for use...

--- campaign-creator-(reboot).rb
+++ campaign-creator-(reboot).rb
@@ -411,16 +411,24 @@ class ModifiedBroadCityAdWordsCampaignFactory < AdWordsCampaignFactory
 end
 
 class BingCampaignFactory < CampaignFactory
-	def createCampaignSitelinks(campaign, niche, seed, short_seed, landingPage, area_of_study, concentration, sitelinks_id)
+	def initialize(opts={})	
+		opts = {location_file_path: "location-data.csv",
+			    id_for_sitelinks: 0}.merge(opts)
+		# Read all locations from file and store as array of arrays
+		@locations = CSV.read(opts[:location_file_path], :headers => true, :encoding => 'windows-1251:utf-8')
+		@id_for_sitelinks = opts[:id_for_sitelinks]
+	end
+
+	def createCampaignSitelinks(campaign, niche, seed, short_seed, landingPage, area_of_study, concentration, sitelink_id)
 		sitelink_page_headline = "Looking for " + seed + "?"
-		sitelink_utm_campaign = "_src*BingAds_d*{ifmobile:mb}{ifnotmobile:dt}_k*{keyword}_m*{matchtype}_c*{creative}_p*{adposition}_n*{network}"
+		sitelink_utm_campaign = "_src*BingAds_d*dt_d2*{IfMobile:mb}{IfNotMobile:dt}_k*{QueryString}_m*{MatchType}_c*{AdId}_p*{adposition}_n*{IfSearch:b}{IfContent:d}"
 		sitelink_destination_url = "http://koodlu.com/#{landingPage}/" +
 									"?area_of_study=#{area_of_study}" + 
 									"&concentration=#{concentration}" +
 									"&seed=#{seed.gsub(" ", "%20")}" +
 									"&headline=#{sitelink_page_headline.gsub(" ","%20").gsub("?","%3f")}" +
 									"&utm_campaign=#{sitelink_utm_campaign}" +
-									"&utm_source=Google" +
+									"&utm_source=Bing" +
 									"&utm_medium=cpc"
 
 		createSitelink1(campaign, niche, seed, short_seed, sitelink_destination_url, sitelink_id)
@@ -552,14 +560,135 @@ class BingCampaignFactory < CampaignFactory
 end
 
 class ModifiedBroadCityStateBingCampaignFactory < BingCampaignFactory
+	def create(seed, short_seed, niche, landingPage, area_of_study, concentration)
+
+		base_campaign_name = "IP=US [#{niche}] {#{seed} +SUBLOCATION +LOCATIONCODE} (search; modbroad)"
+
+		campaigns = Array[]
+		new_campaign = BingCampaign.new( name: base_campaign_name)
+		campaign_counter = 1
+		if @locations.length > BingCampaign::MAX_ADGROUPS_PER_CAMPAIGN
+			new_campaign.name =  base_campaign_name + " Group " + campaign_counter.to_s
+		end
+
+		campaigns << new_campaign 
+
+		current_campaign = campaigns.last
+		createCampaignSitelinks(current_campaign, niche, seed, short_seed, landingPage, area_of_study, concentration, campaign_counter)
+
+
+		@locations.each_with_index do |row, index|
+			# Set some initial variables
+			city = row["City"]
+			state_name = row["State Name"]
+			state_code = row["State Code"]
+
+			location = state_name
+			sublocation = city
+			locationcode = state_code
+
+			page_headline = "Looking for " + seed + " in " + city + ", " + state_code + "?"
+
+			ad_group_name = seed + " in " + city + " " + state_code
+			adgroup = current_campaign.createAdGroup(ad_group_name)
+
+			keywordString = "+" + (seed + " " + city + " " + state_code).gsub(" ", " +")
+			adgroup.createKeyword(keywordString)
+
+			utm_campaign = "_src*BingAds_d*dt_d2*{IfMobile:mb}{IfNotMobile:dt}_k*{QueryString}_m*{MatchType}_c*{AdId}_p*{adposition}_n*{IfSearch:b}{IfContent:d}"
+			destination_url = "http://koodlu.com/#{landingPage}/" +
+							  "?area_of_study=#{area_of_study}" +
+							  "&concentration=#{concentration}" + 
+							  "&seed=#{seed.gsub(" ", "%20")}" + 
+							  "&location=#{location}" +
+							  "&sublocation=#{sublocation.gsub(" ","%20")}" + 
+							  "&locationcode=#{locationcode}" + 
+							  "&headline=#{page_headline.gsub(" ","%20")}" + 
+							  "&utm_campaign=#{utm_campaign}" + 
+							  "&utm_source=Bing" + 
+							  "&utm_medium=cpc"
+			title_options = createHeadlineOptions( niche, seed, short_seed, city, state_code, state_name )
+			text_options = createTextOptions( niche, seed, short_seed, city, state_code, state_name )
+			display_url_options = createDisplayURLOptions( niche, seed, short_seed, city, state_code, state_name )
+			device_preference = "All"
+			createAd(adgroup, title_options, text_options, display_url_options, destination_url, device_preference)
+
+			ad_group_count = index + 1
+			if ad_group_count % BingCampaign::MAX_ADGROUPS_PER_CAMPAIGN == 0
+				campaign_counter += 1
+				campaigns << BingCampaign.new( name: base_campaign_name + " Group " + campaign_counter.to_s )
+				current_campaign = campaigns.last
+				createCampaignSitelinks(current_campaign, niche, seed, short_seed, landingPage, area_of_study, concentration, @id_for_sitelinks)
+			end
+		end
+		return campaigns
+	end
+
+	def createHeadlineOptions( niche, seed, short_seed, city, state_code, state_name )
+		[seed + " " + city + " " + state_code,
+		seed + " " + city,
+		seed + " " + state_name,
+		seed + " " + state_code,
+		seed,
+		short_seed + " " + city + " " + state_code,
+		short_seed + " " + city,
+		short_seed + " " + state_name,
+		short_seed + " " + state_code,
+		short_seed]
+	end
+
+	def createTextOptions( niche, seed, short_seed, city, state_code, state_name )
+		desc_line_1 = [city + " " + seed + " " + state_name + "?",
+		city + " " + seed + " " + state_code + "?",
+		city + " " + seed + "?",
+		state_name + " " + seed + "?",
+		state_code + " " + seed + "?",
+		seed + "?",
+		city + " " + short_seed + " " + state_name + "?",
+		city + " " + short_seed + " " + state_code + "?",
+		city + " " + short_seed + "?",
+		state_name + " " + short_seed + "?",
+		state_code + " " + short_seed + "?",
+		short_seed + "?",]
+
+		desc_line_2 = ["Find " + city + " " + seed,
+		"Find " + state_name + " " + seed,
+	    "Find " + state_code + " " + seed,
+		"Find " + seed,
+		seed,
+		"Find " + city + " " + short_seed,
+		"Find " + state_name + " " + short_seed,
+	    "Find " + state_code + " " + short_seed,
+		"Find " + short_seed,
+		short_seed]
+
+		options = []
+		desc_line_1.each do |line_1|
+			desc_line_2.each do |line_2|
+				options << line_1 + " " + line_2
+			end
+		end
+
+		options
+	end
+
+	def createDisplayURLOptions( niche, seed, short_seed, city, state_code, state_name )
+		[ seed.gsub(" ","-") + ".koodlu.com/" + city.gsub(" ", "-"),
+		seed.gsub(" ","-") + ".koodlu.com/" + state_name.gsub(" ", "-"),
+		seed.gsub(" ","-") + ".koodlu.com/" + state_code,
+		seed.gsub(" ","-") + ".koodlu.com",
+		niche.gsub(" ","-") + ".koodlu.com/" + city.gsub(" ", "-"),
+		niche.gsub(" ","-") + ".koodlu.com/" + state_name.gsub(" ", "-"),
+		niche.gsub(" ","-") + ".koodlu.com/" + state_code,
+		niche.gsub(" ","-") + ".koodlu.com",
+		short_seed.gsub(" ","-") + ".koodlu.com/" + city.gsub(" ", "-"),
+		short_seed.gsub(" ","-") + ".koodlu.com/" + state_name.gsub(" ", "-"),
+		short_seed.gsub(" ","-") + ".koodlu.com/" + state_code,
+		short_seed.gsub(" ","-") + ".koodlu.com"]
+	end
 end
 
 class ModifiedBroadCityBingCampaignFactory < BingCampaignFactory
-	def initialize(opts={})	
-		opts = {location_file_path: "location-data.csv"}.merge(opts)
-		# Read all locations from file and store as array of arrays
-		@locations = CSV.read(opts[:location_file_path], :headers => true, :encoding => 'windows-1251:utf-8')
-	end
 
 	def create(seed, short_seed, niche, landingPage, area_of_study, concentration)
 
@@ -593,7 +722,7 @@ class ModifiedBroadCityBingCampaignFactory < BingCampaignFactory
 			adgroup.createKeyword(keywordString)
 
 
-			utm_campaign = "_src*BingAds_d*{ifmobile:mb}{ifnotmobile:dt}_k*{keyword}_m*{matchtype}_c*{creative}_p*{adposition}_n*{network}"
+			utm_campaign = "_src*BingAds_d*dt_d2*{IfMobile:mb}{IfNotMobile:dt}_k*{QueryString}_m*{MatchType}_c*{AdId}_p*{adposition}_n*{IfSearch:b}{IfContent:d}"
 			destination_url = "http://koodlu.com/#{landingPage}/" +
 							  "?area_of_study=#{area_of_study}" +
 							  "&concentration=#{concentration}" + 
@@ -601,7 +730,7 @@ class ModifiedBroadCityBingCampaignFactory < BingCampaignFactory
 							  "&location=#{sublocation.gsub(" ","%20")}" +
 							  "&headline=#{page_headline.gsub(" ","%20")}" + 
 							  "&utm_campaign=#{utm_campaign}" + 
-							  "&utm_source=Google" + 
+							  "&utm_source=Bing" + 
 							  "&utm_medium=cpc"
 			title_options = createHeadlineOptions( niche, seed, short_seed, city )
 			text_options = createTextOptions( niche, seed, short_seed, city )
@@ -614,7 +743,7 @@ class ModifiedBroadCityBingCampaignFactory < BingCampaignFactory
 				campaign_counter += 1
 				campaigns << BingCampaign.new( name: base_campaign_name + " Group " + campaign_counter.to_s )
 				current_campaign = campaigns.last
-				createCampaignSitelinks(current_campaign, niche, seed, short_seed, landingPage, area_of_study, concentration, campaign_counter)
+				createCampaignSitelinks(current_campaign, niche, seed, short_seed, landingPage, area_of_study, concentration, @id_for_sitelinks)
 			end
 		end
 		return campaigns
@@ -1642,7 +1771,7 @@ bingCampaigns = []
 seeds_file_path = "seeds-for-test-import.csv"
 seeds = CSV.read(seeds_file_path, :headers => true, :encoding => 'windows-1251:utf-8')
 
-seeds.each do |seed_data|
+seeds.each_with_index do |seed_data, index|
 	seed = seed_data["Seed"]
 	short_seed = seed_data["Short Seed"]
 	niche = seed_data["Niche"]
@@ -1690,13 +1819,15 @@ seeds.each do |seed_data|
 
 	# Create Campaign Factory to help with campaign creation
 	if createBingCityState
-		cityStateCampaignFactory = ModifiedBroadCityStateBingCampaignFactory.new(location_file_path: "city-state-location-data.csv")
+		cityStateCampaignFactory = ModifiedBroadCityStateBingCampaignFactory.new(location_file_path: "city-state-location-data.csv",
+																				 id_for_sitelinks: index)
 		bingCampaigns.concat( cityStateCampaignFactory.create(seed, short_seed, niche, landingPage, areaOfStudy, concentration) )
 		puts seed + " Bing City/State Campaign Created"
 	end
 
 	if createBingCity
-		cityCampaignFactory = ModifiedBroadCityBingCampaignFactory.new(location_file_path: "city-location-data.csv")
+		cityCampaignFactory = ModifiedBroadCityBingCampaignFactory.new(location_file_path: "city-location-data.csv",
+																	   id_for_sitelinks: 1000000000 + index)
 		bingCampaigns.concat( cityCampaignFactory.create(seed, short_seed, niche, landingPage, areaOfStudy, concentration) )
 		puts seed + " Bing City Campaign Created"
 	end
